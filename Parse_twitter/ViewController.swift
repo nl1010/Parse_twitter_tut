0c2b25cbd237238869f700f3c835aa64882ac0cf
nl1010 <yanlu1990@hotmail.co.uk>
2014-12-25 19:45:21 +0000

    init

---
+++ Parse_twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parse_twitter
+//
+//  Created by Edward Lucas on 12/25/14.
+//  Copyright (c) 2014 TutorialFederation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
